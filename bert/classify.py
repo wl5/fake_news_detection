2e1d58fe4ee7698c2cfc48e6e64233a5e24046a6
Yuanchu <yuanchu.dang@gmail.com>
2018-12-12 01:20:10 -0500

    Misc revision

--- bert/classify.py
+++ bert/classify.py
@@ -22,9 +22,9 @@ bert_base, vocabulary = nlp.model.get_model('bert_12_768_12',
                                              dataset_name='book_corpus_wiki_en_uncased',
                                              pretrained=True, ctx=ctx, use_pooler=True,
                                              use_decoder=False, use_classifier=False)
-# print(bert_base)
 
 model = BERTClassifier(bert_base, num_classes=2, dropout=0.1)
+
 # only need to initialize the classifier layer.
 model.classifier.initialize(init=mx.init.Normal(0.02), ctx=ctx)
 model.hybridize(static_alloc=True)
@@ -36,17 +36,15 @@ loss_function.hybridize(static_alloc=True)
 metric = mx.metric.Accuracy()
 
 data_train = dataset.DatasetWrapper('test.csv', field_separator = Splitter(','))
-# data_train = dataset.TSVDataset('test.tsv', field_separator = Splitter(','))
 
 sample_id = 0
+
 # sentence a
 print(data_train[sample_id][0])
 # sentence b
 print(data_train[sample_id][1])
 # 1 means equivalent, 0 means not equivalent
 print(data_train[sample_id][2])
-
-import pdb; pdb.set_trace()
 
 # use the vocabulary from pre-trained model for tokenization
 tokenizer = FullTokenizer(vocabulary, do_lower_case=True)
@@ -62,9 +60,7 @@ data_train = data_train.transform(transform)
 # print('segment ids = \n%s'%data_train[sample_id][2])
 # print('label = \n%s'%data_train[sample_id][3])
 
-import pdb; pdb.set_trace()
-
-batch_size = 32
+batch_size = 1
 lr = 5e-6
 bert_dataloader = mx.gluon.data.DataLoader(data_train, batch_size=batch_size,
         shuffle=True, last_batch='rollover')
@@ -75,8 +71,7 @@ trainer = gluon.Trainer(model.collect_params(), 'adam',
 params = [p for p in model.collect_params().values() if p.grad_req != 'null']
 grad_clip = 1
 
-log_interval = 4
-num_epochs = 20
+num_epochs = 3
 
 import pdb; pdb.set_trace()
 
@@ -86,7 +81,7 @@ for epoch_id in range(num_epochs):
     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(bert_dataloader):
         with mx.autograd.record():
 
-            # load data to GPU
+            # load data to CPU or GPU
             token_ids = token_ids.as_in_context(ctx)
             valid_length = valid_length.as_in_context(ctx)
             segment_ids = segment_ids.as_in_context(ctx)
@@ -95,6 +90,7 @@ for epoch_id in range(num_epochs):
             # forward computation
             out = model(token_ids, segment_ids, valid_length.astype('float32'))
             ls = loss_function(out, label).mean()
+            import pdb; pdb.set_trace()
 
         # backward computation
         ls.backward()
@@ -108,5 +104,5 @@ for epoch_id in range(num_epochs):
         step_loss += ls.asscalar()
         metric.update([label], [out])
         # if (batch_id + 1) % (log_interval) == 0:
-        print('[Epoch {} Batch {}/{}] loss={:.4f}, lr={:.7f}, acc={:.3f}'.format(epoch_id, batch_id + 1, len(bert_dataloader),step_loss / log_interval,trainer.learning_rate, metric.get()[1]))
+        print('[Epoch {} Batch {}/{}] loss={:.4f}, lr={:.7f}, acc={:.3f}'.format(epoch_id, batch_id + 1, len(bert_dataloader),step_loss,trainer.learning_rate, metric.get()[1]))
         step_loss = 0
